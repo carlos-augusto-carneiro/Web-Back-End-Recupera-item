5e19b57b1ea25085e6826985682e9e04b0ef969f
carlos-augusto-carneiro <devcarlosaugusto@outlook.com>
2025-07-27 17:12:00 -0300

    Alterados configurações para ser aceito pelo front-end - agora vai2

--- recupera/wait-for-it.sh
+++ recupera/wait-for-it.sh
@@ -2,10 +2,18 @@
 
 set -e
 
-until nc -z -v -w30 postgres 5432
+HOST="$1"
+PORT="$2"
+shift 2
+
+echo "Esperando conexão com $HOST:$PORT..."
+
+until nc -z -v -w30 "$HOST" "$PORT"
 do
-  echo "Aguardando conexão com o PostgreSQL..."
+  echo "Aguardando conexão com o PostgreSQL em $HOST:$PORT..."
   sleep 5
 done
 
-echo "PostgreSQL is up - executing command"
+echo "PostgreSQL está disponível - iniciando aplicação"
+
+exec "$@"
