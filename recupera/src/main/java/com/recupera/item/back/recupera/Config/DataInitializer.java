cdb55e7c4b54b1ad36a2b50a36a328223caaff21
carlos-augusto-carneiro <devcarlosaugusto@outlook.com>
2025-07-29 19:09:57 -0300

    Alteração para o render4

--- recupera/src/main/java/com/recupera/item/back/recupera/Config/DataInitializer.java
+++ recupera/src/main/java/com/recupera/item/back/recupera/Config/DataInitializer.java
@@ -1,4 +1,4 @@
-package com.recupera.item.back.recupera.config;
+package com.recupera.item.back.recupera.Config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
